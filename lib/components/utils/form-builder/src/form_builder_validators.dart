0d98c94d0574bc4be7896cb033b86f57d8d6502e
pluger.creator <102593701+plugcreator2002@users.noreply.github.com>
2022-08-21 11:07:22 +0430

    a

--- lib/components/utils/form-builder/src/form_builder_validators.dart
+++ lib/components/utils/form-builder/src/form_builder_validators.dart
@@ -284,10 +284,10 @@ class FormValidators {
     String? error,
   }) {
     return (candidate) {
-      if (isTrue(candidate?.isNotEmpty) && isTrue(candidate?.isDate)) {
-        return error ?? FormLocalizations.of(context).dateStringError;
+      if (isTrue(candidate?.isDate)) {
+        return null;
       }
-      return null;
+      return error ?? FormLocalizations.of(context).dateStringError;
     };
   }
 }
